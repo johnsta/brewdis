626ca2afab4033bd7f4e39c6ae947e2517fc8e7f
Balan Subramanian <balan@heroku.com>
2020-06-08 19:11:43 -0700

    Tf infra (#4)
    * prevent push of unnecessary files
    
    * added config_server_git_settings
    
    * Added temporary trouble shooting step
    
    * removed troubleshooting step
    
    * Update names
    
    Co-authored-by: Mark Gray <mark,gray@microsoft.com>
    Co-authored-by: Mark Gray (MSFT) <2914336+grayzu@users.noreply.github.com>

---
+++ infra/main.tf
@@ -0,0 +1,32 @@
+resource "azurerm_resource_group" "demo01" {
+    name                      = var.rgName
+    location                  = "eastus2"
+}
+ 
+resource "azurerm_redis_cache" "demo01" {
+  count               = length(var.redisRegions)
+  name                = "brewdis-${var.redisRegions[count.index]}"
+  location            = var.redisRegions[count.index]
+  resource_group_name = azurerm_resource_group.demo01.name
+  capacity            = 2
+  family              = "C"
+  sku_name            = "Standard"
+}
+
+resource "azurerm_spring_cloud_service" "demo01" {
+  name                = var.svcName
+  resource_group_name = azurerm_resource_group.demo01.name
+  location            = "westeurope"
+
+  config_server_git_setting {
+    uri          = "https://github.com/selvasingh/spring-petclinic-microservices-config"
+    label        = "master"
+    search_paths= ["."]
+  }
+}
+
+resource "azurerm_spring_cloud_app" "brew" {
+  name                = var.appName
+  resource_group_name = azurerm_resource_group.demo01.name
+  service_name        = azurerm_spring_cloud_service.demo01.name
+}
