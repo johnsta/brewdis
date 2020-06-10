1e011d9a2a2c407c0712d2617e0903a82f1e6a61
Mark Gray <mark,gray@microsoft.com>
2020-06-10 15:55:28 -0700

    combined infra & app actions and updated names

--- infra/main.tf
+++ infra/main.tf
@@ -1,10 +1,10 @@
-data "azurerm_resource_group" "demo01"  {
-    name              = var.rgName
+data "azurerm_resource_group" "demo01" {
+    name                      = var.rgName
 }
  
 resource "azurerm_redis_cache" "demo01" {
   count               = length(var.redisRegions)
-  name                = "brewdis-${var.redisRegions[count.index]}"
+  name                = "brewredis-${var.redisRegions[count.index]}"
   location            = var.redisRegions[count.index]
   resource_group_name = data.azurerm_resource_group.demo01.name
   capacity            = 2
@@ -12,20 +12,20 @@ resource "azurerm_redis_cache" "demo01" {
   sku_name            = "Standard"
 }
 
-# resource "azurerm_spring_cloud_service" "demo01" {
-#   name                = var.svcName
-#   resource_group_name = data.azurerm_resource_group.demo01.name
-#   location            = "westeurope"
+resource "azurerm_spring_cloud_service" "demo01" {
+  name                = var.svcName
+  resource_group_name = data.azurerm_resource_group.demo01.name
+  location            = "westeurope"
 
-#   config_server_git_setting {
-#     uri               = "https://github.com/selvasingh/spring-petclinic-microservices-config"
-#     label             = "master"
-#     search_paths      = ["."]
-#   }
-# }
+  config_server_git_setting {
+    uri          = "https://github.com/selvasingh/spring-petclinic-microservices-config"
+    label        = "master"
+    search_paths= ["."]
+  }
+}
 
-# resource "azurerm_spring_cloud_app" "brew" {
-#   name                = var.appName
-#   resource_group_name = data.azurerm_resource_group.demo01.name
-#   service_name        = azurerm_spring_cloud_service.demo01.name
-# }
+resource "azurerm_spring_cloud_app" "brew" {
+  name                = var.appName
+  resource_group_name = data.azurerm_resource_group.demo01.name
+  service_name        = azurerm_spring_cloud_service.demo01.name
+}
