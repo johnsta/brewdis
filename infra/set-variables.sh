cdd0b078c420497af98f90223f5ab57947491cc8
balan <balansubr@gmail.com>
2020-06-10 16:43:49 -0700

    simple app changes

--- infra/set-variables.sh
+++ infra/set-variables.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export TF_VAR_redisRegions='["eastus2"]'
-export TF_VAR_rgName="brewdis-dev"
-export TF_VAR_appName="brewdis-app-dev"
-export TF_VAR_svcName="brewdis-svc-dev"
+export TF_VAR_redisRegions='["westeurope"]'
+export TF_VAR_rgName="brewredis-dev"
+export TF_VAR_appName="brewredis-app-dev"
+export TF_VAR_svcName="brewredis-svc-dev"
