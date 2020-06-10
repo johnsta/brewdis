f26e7296008fab91fce9ac673234f388449bb7cf
Mark Gray (MSFT) <2914336+grayzu@users.noreply.github.com>
2020-06-10 14:29:10 -0700

    changed references to data source

--- infra/main.tf
+++ infra/main.tf
@@ -6,7 +6,7 @@ resource "azurerm_redis_cache" "demo01" {
   count               = length(var.redisRegions)
   name                = "brewdis-${var.redisRegions[count.index]}"
   location            = var.redisRegions[count.index]
-  resource_group_name = azurerm_resource_group.demo01.name
+  resource_group_name = data.azurerm_resource_group.demo01.name
   capacity            = 2
   family              = "C"
   sku_name            = "Standard"
@@ -14,7 +14,7 @@ resource "azurerm_redis_cache" "demo01" {
 
 resource "azurerm_spring_cloud_service" "demo01" {
   name                = var.svcName
-  resource_group_name = azurerm_resource_group.demo01.name
+  resource_group_name = data.azurerm_resource_group.demo01.name
   location            = "westeurope"
 
   config_server_git_setting {
@@ -26,6 +26,6 @@ resource "azurerm_spring_cloud_service" "demo01" {
 
 resource "azurerm_spring_cloud_app" "brew" {
   name                = var.appName
-  resource_group_name = azurerm_resource_group.demo01.name
+  resource_group_name = data.azurerm_resource_group.demo01.name
   service_name        = azurerm_spring_cloud_service.demo01.name
 }
