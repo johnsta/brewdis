eeed78fb9d84700791133936d7aa93f373b2d838
Mark Gray (MSFT) <2914336+grayzu@users.noreply.github.com>
2020-06-10 14:56:17 -0700

    Temp comment out SC
    Commenting out Spring cloud so that it will be deleted.

--- infra/main.tf
+++ infra/main.tf
@@ -12,20 +12,20 @@ resource "azurerm_redis_cache" "demo01" {
   sku_name            = "Standard"
 }
 
-resource "azurerm_spring_cloud_service" "demo01" {
-  name                = var.svcName
-  resource_group_name = data.azurerm_resource_group.demo01.name
-  location            = "westeurope"
+# resource "azurerm_spring_cloud_service" "demo01" {
+#   name                = var.svcName
+#   resource_group_name = data.azurerm_resource_group.demo01.name
+#   location            = "westeurope"
 
-  config_server_git_setting {
-    uri               = "https://github.com/selvasingh/spring-petclinic-microservices-config"
-    label             = "master"
-    search_paths      = ["."]
-  }
-}
+#   config_server_git_setting {
+#     uri               = "https://github.com/selvasingh/spring-petclinic-microservices-config"
+#     label             = "master"
+#     search_paths      = ["."]
+#   }
+# }
 
-resource "azurerm_spring_cloud_app" "brew" {
-  name                = var.appName
-  resource_group_name = data.azurerm_resource_group.demo01.name
-  service_name        = azurerm_spring_cloud_service.demo01.name
-}
+# resource "azurerm_spring_cloud_app" "brew" {
+#   name                = var.appName
+#   resource_group_name = data.azurerm_resource_group.demo01.name
+#   service_name        = azurerm_spring_cloud_service.demo01.name
+# }
