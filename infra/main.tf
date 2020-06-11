def12f61fb83ce785cbbe9ab74180a7f499afefc
balan <balansubr@gmail.com>
2020-06-11 13:00:42 -0700

    Baseline on no app config dependency

--- infra/main.tf
+++ infra/main.tf
@@ -1,12 +1,13 @@
-data "azurerm_resource_group" "demo01" {
+resource "azurerm_resource_group" "demo01" {
     name                      = var.rgName
+    location                  = "westeurope"
 }
  
 resource "azurerm_redis_cache" "demo01" {
   count               = length(var.redisRegions)
   name                = "brewredis-${var.redisRegions[count.index]}"
   location            = var.redisRegions[count.index]
-  resource_group_name = data.azurerm_resource_group.demo01.name
+  resource_group_name = azurerm_resource_group.demo01.name
   capacity            = 2
   family              = "C"
   sku_name            = "Standard"
@@ -14,7 +15,7 @@ resource "azurerm_redis_cache" "demo01" {
 
 resource "azurerm_spring_cloud_service" "demo01" {
   name                = var.svcName
-  resource_group_name = data.azurerm_resource_group.demo01.name
+  resource_group_name = azurerm_resource_group.demo01.name
   location            = "westeurope"
 
   config_server_git_setting {
@@ -26,6 +27,6 @@ resource "azurerm_spring_cloud_service" "demo01" {
 
 resource "azurerm_spring_cloud_app" "brew" {
   name                = var.appName
-  resource_group_name = data.azurerm_resource_group.demo01.name
+  resource_group_name = azurerm_resource_group.demo01.name
   service_name        = azurerm_spring_cloud_service.demo01.name
 }
