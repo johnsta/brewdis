893e71af34fcd70bcc368f74a6d2ec598504f025
balan <balansubr@gmail.com>
2020-06-11 09:36:55 -0700

    Update variables for tf template

--- infra/set-variables.sh
+++ infra/set-variables.sh
@@ -2,5 +2,5 @@
 
 export TF_VAR_redisRegions='["westeurope"]'
 export TF_VAR_rgName="brewredis-dev"
-export TF_VAR_appName="brewredis-app-dev"
-export TF_VAR_svcName="brewredis-svc-dev"
+export TF_VAR_appName="storefront-dev"
+export TF_VAR_svcName="brewredis-spring-cloud-dev"
